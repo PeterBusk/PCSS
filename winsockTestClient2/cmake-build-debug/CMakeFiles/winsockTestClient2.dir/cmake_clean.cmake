1662fe8cc0b19f3589fa9073ffcc8191b5a09dff
PeterBusk <36533756+PeterBusk@users.noreply.github.com>
2018-12-23 19:41:50 +0100

    Cmake for client no 2...

---
+++ winsockTestClient2/cmake-build-debug/CMakeFiles/winsockTestClient2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/winsockTestClient2.dir/main.cpp.obj"
+  "winsockTestClient2.pdb"
+  "winsockTestClient2.exe"
+  "winsockTestClient2.exe.manifest"
+  "winsockTestClient2.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/winsockTestClient2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
