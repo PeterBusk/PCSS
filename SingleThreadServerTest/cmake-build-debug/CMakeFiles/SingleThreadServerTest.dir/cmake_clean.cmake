d3c340cbdb79dc5340ce2b31ecbe55f62ad8c758
PeterBusk <36533756+PeterBusk@users.noreply.github.com>
2018-12-23 19:34:48 +0100

    Single threaded server make files

---
+++ SingleThreadServerTest/cmake-build-debug/CMakeFiles/SingleThreadServerTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SingleThreadServerTest.dir/main.cpp.obj"
+  "SingleThreadServerTest.pdb"
+  "SingleThreadServerTest.exe"
+  "SingleThreadServerTest.exe.manifest"
+  "SingleThreadServerTest.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SingleThreadServerTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
